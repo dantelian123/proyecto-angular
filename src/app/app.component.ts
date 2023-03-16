c53c923f9dbb35317634df73743a5428e73df1cc
dantelian123 <75514187+dantelian123@users.noreply.github.com>
2023-03-16 09:53:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { JsonFormData } from './components/json-form/json-form.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public formData:any;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('../assets/form.json')
+    .subscribe((formData)=>{
+      console.log(formData);
+      this.formData = formData;
+      console.log(this.formData);
+    })
+  }
   title = 'Formulario';
 }
